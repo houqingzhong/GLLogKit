8feba01054a3041e0772b25c109048bf2214f33e
Gang Li <li.tzuhsien@outlook.com>
2015-01-26 16:04:08 +0800

    Ignored xcuserdata

--- GLLogKit/GLLogKit/GLLoggerImplementation.m
+++ GLLogKit/GLLogKit/GLLoggerImplementation.m
@@ -8,7 +8,59 @@
 
 #import "GLLoggerImplementation.h"
 
+NSString * getStringFromValue(GLLoggingLevel level) {
+    NSString *valueString = nil;
+    switch (level) {
+        case GLLoggingLevelInfo:
+            valueString = @"INFO";
+            break;
+        case GLLoggingLevelTrace:
+            valueString = @"TRACE";
+            break;
+        case GLLoggingLevelDebug:
+            valueString = @"DEBUG";
+            break;
+        case GLLoggingLevelWarn:
+            valueString = @"WARN";
+            break;
+        case GLLoggingLevelError:
+            valueString = @"ERROR";
+            break;
+        case GLLoggingLevelFatal:
+            valueString = @"FATAL";
+            break;
+        default:
+            break;
+    }
+    return valueString;
+}
 
+GLLoggingLevel getValueFromString(NSString *string) {
+    GLLoggingLevel level = GLLoggingLevelUndefined;
+    switch (string.UTF8String[0]) {
+        case 'I':
+            level = GLLoggingLevelInfo;
+            break;
+        case 'T':
+            level = GLLoggingLevelTrace;
+            break;
+        case 'D':
+            level = GLLoggingLevelDebug;
+            break;
+        case 'W':
+            level = GLLoggingLevelWarn;
+            break;
+        case 'E':
+            level = GLLoggingLevelError;
+            break;
+        case 'F':
+            level = GLLoggingLevelFatal;
+            break;
+        default:
+            break;
+    }
+    return level;
+}
 
 @implementation GLLoggerImplementation
 
